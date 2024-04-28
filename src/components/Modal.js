0a5f468aa0d8a596b1bfcd34bea2e8346bbe63db
Rutuja Harne <80842769+rutujaharne23@users.noreply.github.com>
2024-04-29 02:17:17 +0530

    Added HTML code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,8 +12,8 @@ export const Modal = ({ activeImage, imageGallery, imageUrl, imageName, isOpen, 
             <div className="fixed inset-0 bg-gray-700 bg-opacity-50 z-40"></div>
             
             {/* Modal content */}
-            <div className="bg-white w-1/2 p-3 shadow-xl relative z-50">
-                <span className="cursor-pointer absolute top-0 right-0 p-2" onClick={isClose}>X</span>
+            <div className="bg-white p-3 shadow-xl relative z-50">
+                <span className="cursor-pointer absolute top-0 right-0 p-3" onClick={isClose}>&#10006;</span>
                 <p className="p-2">{imageName}</p>
                 <Slider sliderUrl={imageUrl} activeSlider={activeImage} sliderImages={imageGallery} />
             </div>
